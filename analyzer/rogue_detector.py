6433e50f21ecf68f5ee4546c00840434393ee1cf
sabari <151620115+sabari038@users.noreply.github.com>
2025-07-19 21:48:29 +0530

    Delete analyzer directory

--- analyzer/rogue_detector.py
+++
@@ -1,8 +0,0 @@
-from config import WHITELIST_DEVICES
-
-def detect_rogue_devices(discovered_devices):
-    rogue = []
-    for device in discovered_devices:
-        if device not in WHITELIST_DEVICES:
-            rogue.append(device)
-    return rogue
